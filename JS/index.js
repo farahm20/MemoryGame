a98b21ceb213147fcbd146f0760def9d42c9594e
jensengbg-Farah-Mahboob <45151545+farahm20@users.noreply.github.com>
2020-03-14 23:53:16 +0100

    Fixed shuufle and match cards . added score and refresh button.

--- JS/index.js
+++ JS/index.js
@@ -1,148 +1,131 @@
-//different ways to get an html collection of elements
 let numArray = [];
+var allNumberCards = null;
+let allCards = document.querySelectorAll('.memoryCard');
+let score = 0;
 shuffleCards();
+displayScore();
+refreshGame();
+function shuffleCards(){  
+    allNumberCards = document.querySelectorAll('.cardNumber');
 
-//checkForMatch();
-
-
-
-let getAllButtons = document.querySelectorAll('.button');
-
-let index=0;
-getAllButtons.forEach((Element)=>{
-    console.log(Element)
-    flipCards()
-    index++})   
-
-
-function flipCards()
-{
-
-    document.addEventListener("DOMContentLoaded", function(event) {
-        document.getElementById("flip-card-to-back").style.visibility ="visible";
-        document.getElementById("flip-card-to-front").style.visibility ="visible";
-    
-        document.getElementById("flip-card-to-back").onclick = function() {
-        document.getElementById("flip-card").classList.toggle("do-flip");
-        };
-    
-        document.getElementById("flip-card-to-front").onclick = function() {
-        document.getElementById("flip-card").classList.toggle("do-flip");
-        };
-    });
-}    
-
-/*
-//ateempt 1
-var refreshIt = document.getElementById("refresh");
-refreshIt.addEventListener("click", shuffleCards);
-*/
-
-/*
-//attempt2
-var buttonOn = document.getElementById("front");
-var buttonOff = document.getElementById("back");
-
-document.getElementById("buttonOnclick").onclick = function(){
-    myFunction()
-};
-
-function myFunction(){
-    document.getElementById("back").classList.toggle("show");
-}
-
-
-document.getElementById("refresh1").onclick = function() {
-    refresh1.innerHTML = "Im refreshing";
-    //   shuffleCards()
-};
-
-document.getElementById("score1").onclick = function() {
-    score1.innerHTML = "You win";
-};
-*/
-
-function shuffleCards()
-{
-    numArray = shuffleArray();
-
-    console.log("shuffleArray function: " + numArray);
-
-    var allBackButtons = document.querySelectorAll('.toChange');
-    console.log(allBackButtons);
-    let i=0;
-    allBackButtons.forEach(function(buttonValue){
-            buttonValue.innerHTML = '' + numArray[i];   
-        console.log(buttonValue.textContent);
-        i++;
-      
-
-    })
-}
-
-function checkForMatch()
-{
-    let score = 0;
-    console.log("Checking for a Match");
-
-    var buttonsCheck = document.querySelectorAll('.toChange');
-    let i = 0;
-    let button1;
-    let button2;
-    buttonsCheck.forEach(function(buttonValue){
-        let button1 = buttonValue.textContent;
-        console.log("Value in button1: " + button1);
-        
-        let button2 = buttonValue.textContent;
-        console.log("Value in button2: " + button2);        
-    })
-   
-
-}
-
-
-
-//bringOverLay();  
-
-
-function shuffleArray()
-{
-    numArray = generateArray();
-    numArray = copyArray();
-
-    newLength = numArray.length - 1;
+    let newLength = allNumberCards.length - 1;
     let newIndex, temp;
-        for (let j = newLength; j >= 0; j-- )
-        {
-            newIndex = Math.floor(Math.random()* (j+1));
-            temp = numArray[j];
-            numArray[j] = numArray[newIndex];
-            numArray[newIndex] = temp;
-        }
-    return numArray;
-}
-
-function generateArray()
-{
-  for(let i = 0; i < 8; i ++)
-  {
-    let randomNum = (Math.ceil((Math.random()) * 9));
-    while(numArray.lastIndexOf(randomNum) !== -1)//returns -1 if number not found. 
+       
+    for (var j = newLength; j >= 0; j-- )
     {
-      randomNum = (Math.ceil((Math.random()) * 9));
-    }  
-    numArray.push(randomNum);
-  }
-  return numArray;
-}
-
-function copyArray(){
-  let length = (numArray.length-1);
-    for(var i = 0; i <= length; i++)
-    {
-      numArray.push(numArray[i]);
+        newIndex = Math.floor(Math.random()* (j+1));
+        temp = allNumberCards[j].innerHTML;
+        allNumberCards[j].innerHTML = allNumberCards[newIndex].innerHTML;
+        allNumberCards[newIndex].innerHTML = temp;
     }
-   return numArray;
+
+    for(let i = 0; i < allNumberCards.length; i++)
+    {
+        allCards[i].dataset.number = allNumberCards[i].innerHTML;
+        console.log("Dataset number: " + allCards[i].dataset.number);    
+
+    }
+}
+
+//selecting all the cards and saving them in a nodeList
+//defining the click and calling function flipCard to flip each card
+
+let cardHasFlipped = false; /*shows js has flipped card. will turn true if card flipped */
+let lockOtherCards = false; 
+let firstCardFlipped;
+let secondCardFlipped;
+
+//accessing the card that was clicked with this. 
+//further access to elements classlist
+function flipCard() {
+
+    //if its true, dont flip more cards
+    if (lockOtherCards) 
+        return;
+    
+    //doesnt allow d´same card to flip twice and match with itself
+    if(this === firstCardFlipped)
+        return;
+    
+
+    this.classList.add('flip'); //adds a flip
+
+    /**If no card flipped, flip clicked card and set flip to true */
+    if(!cardHasFlipped) {
+        cardHasFlipped = true;
+        firstCardFlipped = this;
+        return;
+    }
+    
+    secondCardFlipped = this;
+    lockOtherCards = true;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+
+//    let isMatch = firstCardFlipped.dataset.number === secondCardFlipped.dataset.number;
+//    isMatch ? removeTheFlip() : turnCardsBack();
+    if(firstCardFlipped.dataset.number === secondCardFlipped.dataset.number) {
+        removeTheFlip();
+        score ++;
+        console.log("Score " + score);
+        if(score === 8)
+        {
+            bringOverLay();
+        }
+    }
+    else{
+        turnCardsBack();
+    }
+
+}
+
+function removeTheFlip() {
+    firstCardFlipped.removeEventListener('click', flipCard);
+    secondCardFlipped.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function turnCardsBack () {
+    //    lockOtherCards = true; /*lock cards until two selected cards turn back */
+    setTimeout(() => {
+        firstCardFlipped.classList.remove('flip');
+        secondCardFlipped.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard(){
+    firstCardFlipped = null;
+    secondCardFlipped = null;
+    cardHasFlipped = false;
+    lockOtherCards = false;
+    
+}
+allCards.forEach(card => card.addEventListener('click', flipCard)); 
+
+function refreshGame() {
+   let refrehButton = document.getElementById('refresh');
+   refrehButton.addEventListener('click', function() {
+  //     shuffleCards();
+        setTimeout(location.reload(true),t)
+   });  
+}
+
+function displayScore() {
+   let scoreButton = document.getElementById('score');
+   scoreButton.addEventListener('mouseover', scoreFunction);
+   scoreButton.addEventListener('mouseout', pointsFunction);
+   
+   function scoreFunction() {
+       scoreButton.innerHTML = score;
+   }
+   
+   function pointsFunction() {
+       scoreButton.innerHTML = "Check Score";
+   }
 }
 
 function bringOverLay()
@@ -154,35 +137,4 @@ function bringOverLay()
         overLay.style.display = "block";
     }
 }
-/*
 
-var allBackButtons = document.querySelectorAll('.toChange');
-console.log(allBackButtons);
-
-allBackButtons.forEach(function(buttonValue){
-    buttonValue.textContent = 'testing';
-    console.log(buttonValue.textContent);
-})
-
-
-let allBackButtons = document.getElementsByClassName('toChange');
-
-Array.from(allBackButtons).forEach(function(item){
-    console.log(item);
-})
-
-
-for(let i = 0; i < allBackButtons.length; i++) {
-    console.log(' ' + allBackButtons[i].innerHTML.valueOf('h1'));
-    console.log(allBackButtons[i]);
-}
-
-for(let i = 0; i < allBackButtons.length; i++) {
-    let buttonValue = 'ZZZ';
-
-    document.getElementsByClassName('toChange').innerHTML = buttonValue;
-    console.log(' ' + allBackButtons[i].innerHTML.valueOf('h1'));
-    console.log(allBackButtons[i]); 
-}
-
-*/
